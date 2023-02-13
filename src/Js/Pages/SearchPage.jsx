14ecb5005bb90f0f2e0c680160a9ba4cd0473764
tush2207 <tushar.v2stech@gmail.com>
2023-02-13 12:57:53 +0530

    Selected Drawer

--- src/Js/Pages/SearchPage.jsx
+++ src/Js/Pages/SearchPage.jsx
@@ -3,11 +3,10 @@ import { Card, CardContent, Grid, Stack, Typography } from "@mui/material";
 import { teal } from "@mui/material/colors";
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { COUNTRYDETAILS } from "../../Utils/Constants/api_constants";
+import { COUNTRYDETAIL } from "../../Utils/Constants/api_constants";
 
 const SearchPage = ({ data }) => {
   const navigate = useNavigate();
-  console.log("data", data);
   useEffect(() => {}, [data]);
 
   return (
@@ -20,7 +19,7 @@ const SearchPage = ({ data }) => {
               sx={{ width: "50%", cursor: "pointer", marginTop: "85px" }}
               onClick={() =>
                 data?.name?.common !== "" || data?.name?.common !== undefined
-                  ? navigate(COUNTRYDETAILS)
+                  ? navigate(COUNTRYDETAIL + data?.name?.common)
                   : ""
               }
             >
